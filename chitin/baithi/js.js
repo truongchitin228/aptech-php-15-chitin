4bc3a4350aa406f6fc643969d93d3aa995464156
unknown <truongchitin2208@gmail.com>
2018-10-24 11:42:49 +0700

    buoi 1 php

---
+++ chitin/baithi/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#js-navigation-button").click(function() {
+        $("#js-navigation").slideToggle();
+
+    });
+
+});
+$('#btn').click(function() {
+    $('#nav').toggleClass('shadow')
+});
---
+++ php/22-10-2018/tin.php
@@ -0,0 +1,6 @@
+<?php
+$ten = "T88in";
+$tuoi = "18";
+echo "<h1>truong chi $ten</h1>" ;
+
+?>
